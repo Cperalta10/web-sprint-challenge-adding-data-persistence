e3764f3f702fcf06c89672ae092131f331f9a486
Christopher Peralta <chrisperalta1998@gmail.com>
2022-08-19 16:54:59 -0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const PORT = process.env.PORT || 9000;
+const server = require("./api/server");
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
