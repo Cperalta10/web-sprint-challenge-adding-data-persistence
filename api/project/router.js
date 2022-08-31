654a5c801d45e02c67a7e740c490bfee7c22743f
Christopher Peralta <chrisperalta1998@gmail.com>
2022-08-31 15:51:23 -0700

    finished mvp

--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,44 @@
 // build your `/api/projects` router here
+
 const express = require("express");
+const project = require("./model");
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  await project
+    .getAll()
+    .then((projects) => {
+      projects.map((project) => {
+        project.project_completed === 0
+          ? (project.project_completed = false)
+          : (project.project_completed = true);
+      });
+      res.status(200).json(projects);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  project
+    .add(req.body)
+    .then((newProject) => {
+      if (newProject[newProject.length - 1].project_completed == 0) {
+        newProject[newProject.length - 1].project_completed = false;
+        res.status(201).json(newProject[newProject.length - 1]);
+      } else {
+        newProject[newProject.length - 1].project_completed = true;
+        res.status(201).json(newProject[newProject.length - 1]);
+      }
+    })
+    .catch(next);
+});
+
 module.exports = router;
+
+// - [ ] `[POST] /api/projects`
+//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+//   - Example of response body: `{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}`
+
+// - [ ] `[GET] /api/projects`
+//   - Even though `project_completed` is stored as an integer, the API uses booleans when interacting with the client
+//   - Example of response body: `[{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]`
--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 // build your server here and require it from index.js
+
 const express = require("express");
+const helmet = require("helmet");
+
 const server = express();
-const resourceRouter = require("./resource/router");
-const projectRouter = require("./project/router");
-const taskRouter = require("./task/router");
+const Project = require("./project/router.js");
+const Resource = require("./resource/router.js");
+const Tasks = require("./task/router.js");
 
+server.use(helmet());
 server.use(express.json());
+server.use("/api/projects", Project);
+server.use("/api/resources", Resource);
+server.use("/api/tasks", Tasks);
 
-server.use("/api/resources", resourceRouter);
-server.use("/api/projects", projectRouter);
-server.use("/api/tasks", taskRouter);
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = server;
